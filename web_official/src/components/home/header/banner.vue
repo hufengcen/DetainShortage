d653b6d7f85b1e37e445315ff4eb3ea0b14332cb
hufengcen <hufengcen@fclassroom.com>
2018-07-20 18:06:26 +0800

    pc 主页修改

--- web_official/src/components/home/header/banner.vue
+++ web_official/src/components/home/header/banner.vue
@@ -1,37 +1,103 @@
 <template>
   <section>
-    <div class="banner">
-      <img src="//hbfile.b0.upaiyun.com/img/home/banner/cdf0a6ab627bc33af759533af558a118be5a1b1dd2d93" alt="">
+    <div class="banner" id="map_container">
+      <!-- <img src="//hbfile.b0.upaiyun.com/img/home/banner/cdf0a6ab627bc33af759533af558a118be5a1b1dd2d93" alt=""> -->
       <!-- <iframe src="https://map.baidu.com/" width="100%" height="800"></iframe> -->
     </div>
+    <VInfoList></VInfoList>
   </section>
 </template>
 
 <script>
+
 export default {
   name: 'banner',
+  components: {
+    VInfoList (resolve) {
+      require(['./infoList'], resolve)
+    }
+  },
   data () {
     return {
-      msg: ''
+      map: null
     }
+  },
+  created () {
+
+  },
+  methods: {
+    mapInit () {
+      let map = this.map = new AMap.Map('map_container', {
+        resizeEnable: true,
+        zoom: 5,  //设置地图显示的缩放级别
+        // layers: [new AMap.TileLayer.Satellite()],  //设置图层,可设置成包含一个或多个图层的数组
+        // mapStyle: 'amap://styles/whitesmoke',  //设置地图的显示样式
+        viewMode: '2D',  //设置地图模式
+        lang:'zh_cn',  //设置地图语言类型
+      })
+      map.plugin('AMap.Geolocation', function () {
+        var geolocation = new AMap.Geolocation({
+          // 是否使用高精度定位，默认：true
+          enableHighAccuracy: true,
+          // 设置定位超时时间，默认：无穷大
+          timeout: 10000,
+          // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
+          buttonOffset: new AMap.Pixel(10, 20),
+          //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+          zoomToAccuracy: true,
+          //  定位按钮的排放位置,  RB表示右下
+          buttonPosition: 'RB'
+        })
+        map.addControl(geolocation)
+        geolocation.getCurrentPosition()
+        AMap.event.addListener(geolocation, 'complete', onComplete)
+        AMap.event.addListener(geolocation, 'error', onError)
+
+        function onComplete (data) {
+          // data是具体的定位信息
+        }
+
+        function onError (data) {
+          // 定位出错
+        }
+      })
+    },
+    setMarkers () {
+      var marker = new AMap.Marker({
+        position: new AMap.LngLat(120.662586, 31.293365),
+        title: '苏州',
+        content: '<div class="map_marker"><img src="http://img.hb.aicdn.com/16af5bfcd0f50f7d59e7ccb263f93ec755828b41644aa-CpjD4d_sq320"/></div>'
+      })
+      AMap.event.addListener(marker, "click", function(e) {
+  			alert('你准备查看当前信息')
+  		})
+      this.map.add(marker)
+      this.map.setFitView()
+    },
+    search () {
+
+    }
+  },
+  mounted () {
+    this.mapInit()
+    this.setMarkers()
+    this.search()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style>
+.map_marker{
+  background: url('./img/map_marker.png') 0 0 no-repeat;
+  background-size: contain;
+  width: 80px;height: 80px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.map_marker img{
+  width:36px;height:36px;border-radius:50%;overflow:hidden;border:2px solid #fff;margin-top: 8px
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.banner {
+  height: 800px
 }
 </style>
---
+++ web_official/src/components/home/header/infoList.vue
@@ -0,0 +1,65 @@
+<template lang="html">
+  <div class="infoList">
+    <div class="info">
+      <div class="header">
+        <img src="http://img.hb.aicdn.com/6e5934abb18804e0a574a80f17b9cc708559a55360fb-VdcTL4_sq320" alt="">
+        <strong>{{this.nick_name}}</strong>
+      </div>
+      <div class="">
+        
+      </div>
+    </div>
+    <div class="activity">
+      <div class="">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'infoList',
+  data() {
+    return {
+      header_img: '',
+      nick_name: '来自星星的狗',
+      activity: '',
+      intro: '',
+      content: '',
+      desc: ''
+    }
+  },
+  created: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="css" scope>
+.infoList {
+  background: rgba(0, 0, 0, 0.7);
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  position: fixed;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between
+}
+.infoList .info{
+  width: 50%;
+  margin-top: 50px;
+}
+.infoList .info .header img{
+  width: 180px;height: 180px
+}
+.infoList .info .header strong{
+  color: #fff;
+  font-size: 36px
+}
+</style>
